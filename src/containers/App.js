44759d1e66fbbe58408b300afbe75ced5660328e
Piash <nazem.piash10@gmail.com>
2020-12-07 01:44:14 +0600

    app js convert to class based component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,86 +1,89 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Cockpit from '../components/Cockpit/Cockpit';
 import Persons from '../components/Persons/Persons';
 import Radium, { StyleRoot } from 'radium';
 
-function App() {
+class App extends Component {
   // All state variables
-  // State 1
-  const [persons, setPersons] = useState(
-    [
+  state = {
+    persons: [
       { id: 'asadsa1', name: "Max", age:"12" }, 
       { id: 'asadsa2', name: "Jack", age:"24" }, 
-      { id: 'asadsa3', name: "John", age:"30" }, 
-    ]
-  );
-
-  // State 2
-  const [showPersons, setShowPersons] = useState(false)
-  
-  // change value of State 1
-  const switchName = (newName) => {
-    // alert('Clicked!! ', newName);
-    setPersons([
-      { id: 'asadsa1', name: newName, age:"17" }, 
-      { id: 'asadsa2', name: "Jack Ryan", age:"26" }, 
-      { id: 'asadsa3', name: "John Mark", age:"34" }, 
-    ]);
+      { id: 'asadsa3', name: "John", age:"30" },
+    ],
+    otherState: 'some other value',
+    showPersons: false
   }
-// change value of State 1: two way binding
-  const nameChange = (event, id) => {
-    const personIndex = persons.findIndex( p => p.id === id);
-    const person = {  ...persons[personIndex] };
+
+  // change value of State 1: two way binding
+  nameChange = (event, id) => {
+    const personIndex = this.state.persons.findIndex( p => p.id === id);
+    const person = {  ...this.state.persons[personIndex] };
     // another approach
     // const person = Object.assign({}, persons[personIndex]);
     person.name = event.target.value;
-    const allpersons = [...persons];
+    const allpersons = [...this.state.persons];
     allpersons[personIndex] = person;
-    setPersons(allpersons);
+    this.setState({persons: allpersons});
   }
 
-
- // using IF condition: Show hide section
-  const togglePerson = () => {
-    const show = showPersons;
-    setShowPersons(!show); 
-  };
-
   // Use of state with list dynamically: delete action: 
-  const deletePerson = (personIndex) => {
-    const allpersons = [...persons];
+  deletePerson = (personIndex) => {
+    const allpersons = [...this.state.persons];
     allpersons.splice(personIndex, 1);
-    setPersons(allpersons);
+    this.setState({persons: allpersons});
+  
     console.log(allpersons);
   }
 
-  let allPersons = null;
+  // using IF condition: Show hide section
+  togglePerson = () => {
+    const show = this.state.showPersons;
+    this.setState({showPersons: !show}); 
+  };
+  
+  // change value of State 1
+  switchName = (newName) => {
+    // alert('Clicked!! ', newName);
+    const person = [
+      { id: 'asadsa1', name: newName, age:"17" }, 
+      { id: 'asadsa2', name: "Jack Ryan", age:"26" }, 
+      { id: 'asadsa3', name: "John Mark", age:"34" }, 
+    ];
+    this.setState({persons: person});
+  }
+  
+  render(){
+    let allPersons = null;
+
+    if(this.state.showPersons) {
+      allPersons = <Persons 
+            persons={this.state.persons}
+            deletePerson={this.deletePerson}
+            nameChange={this.nameChange}
+          />
+        ; 
+    }
   /**
    * if i call a method like this: click={() => deletePerson(index)}
    * no need to bind like following: 
    * click={switchName.bind(this, 'Bal')}
    */
-  if(showPersons) {
-    allPersons = <Persons 
-          persons={persons}
-          deletePerson={deletePerson}
-          nameChange={nameChange}
-        />
-      ; 
+    return (
+      <StyleRoot>
+        <div className="App">
+          <Cockpit 
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            togglePerson={this.togglePerson}
+          />
+          {allPersons }
+        </div>
+      </StyleRoot>
+    );
   }
-
-  return (
-    <StyleRoot>
-      <div className="App">
-        <Cockpit 
-          showPersons={showPersons}
-          persons={persons}
-          togglePerson={togglePerson}
-        />
-        {allPersons }
-      </div>
-    </StyleRoot>
-  );
+  
 }
 
 export default Radium(App) ;

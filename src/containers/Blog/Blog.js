4f7f473297b0f1e402a37e40a62a6956e32f38a2
Piash <nazem.piash10@gmail.com>
2020-12-27 05:24:01 +0600

    route4: lazy route basic

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,8 +3,12 @@ import { NavLink, Route, Switch } from "react-router-dom";
 
 import './Blog.css';
 import Posts from "./Posts/Posts";
-import NewPost from '../../components/NewPost/NewPost';
+// import NewPost from '../../components/NewPost/NewPost';
+import asynchComponent from "../../hoc/asynchComponent";
 
+const AsynchNewPost = asynchComponent(() => {
+    return import("../../components/NewPost/NewPost");
+})
 class Blog extends Component {
     render () {
         return (
@@ -32,7 +36,7 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
                     <Route path="/posts" component={Posts} />
-                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/new-post" component={AsynchNewPost} />
                     
                 </Switch>
             </div>
